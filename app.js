d66dc4eba67417d7b7284256fa06b1061606f5dd
haibin <sunhaibin@goyoo.com>
2015-09-14 13:07:38 +0800

    update css

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var favicon = require('serve-favicon');
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var config = require('./config');
+var config = require('./server/config');
 
-var rootPath = path.resolve(__dirname + '/../');
+var rootPath = path.resolve(__dirname);
+console.log(rootPath);
 
 // Returns an Express server
 var server = jsonServer.create();
