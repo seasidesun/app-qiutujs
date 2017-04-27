6827ce3b3ffa43c0cb562b71c5a7bb1711a335e8
seasidesun <seasidetank@gmail.com>
2017-04-27 19:35:44 +0800

    up the movements list

--- app/main.js
+++ app/main.js
@@ -5,8 +5,12 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './components/App'
 import Style from './style.less'
 
+(function () {
+    document.getElementsByTagName('html')[0].style.fontSize = (75/375) * window.screen.width + 'px';
+})();
+
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
   </Router>
-), document.getElementById('app'))
+), document.getElementById('app'));
