7f36604affd9391da4b69b2801d894e2e1b3b30d
haibinsun <seasidetank@gmail.com>
2019-04-13 10:50:19 +0800

    refactor: rm the useless files

--- src/pages/bar/index.js
+++ src/pages/bar/index.js
@@ -1,3 +1,9 @@
-import '@/styles/h5.css'
-import '@/styles/base.css'
-import '@/compontents/header/header.js'
+import '@/styles/h5.scss'
+import '@/styles/base.scss'
+
+import '@/compontents/header'
+import '@/compontents/movements'
+
+import footer from '@/compontents/footer'
+
+document.getElementById('footer-template').innerHTML = footer
--- src/pages/baz/index.js
+++
@@ -1,6 +0,0 @@
-import '@/styles/base.css'
-import '@/compontents/header/header.js'
-
-import footer from '@/compontents/footer/footer.js'
-
-document.getElementById('footer-template').innerHTML = footer
