d6e3ff20e664d25623fe5ccbe875755123f3a418
haibinsun <seasidetank@gmail.com>
2019-04-14 15:45:50 +0800

    feat: up the route transition

--- src/pages/index/app.vue
+++ src/pages/index/app.vue
@@ -1,16 +1,28 @@
 <template>
-    <router-view></router-view>
+    <transition :name="aniName">
+        <router-view></router-view>
+    </transition>
 </template>
 
 <script>
+const routerAniMap = ['index-movement', 'movement-step', 'index-step']
+
 export default {
     name: 'App',
     compontents: {
     },
     data () {
         return {
+            aniName: 'forward',
         }
-    }
+    },
+    watch: {
+        '$route' (to, from) {
+            if (!from || !to) this.aniName = 'forward'
+            else if (routerAniMap.includes(`${from.name}-${to.name}`)) this.aniName = 'forward'
+            else this.aniName = 'back'
+        },
+    },
 }
 </script>
 <style lang="scss">
@@ -19,5 +31,20 @@ export default {
 @import "~@/styles/normalize.css";
 </style>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.back-leave-active, .back-enter-active,
+.forward-leave-active, .forward-enter-active {
+    position: fixed;
+    top: 0;
+    transition: transform .5s, opacity 1s;
+}
+.forward-leave-to, .back-enter {
+    transform: translate(-100%);
+}
+.forward-enter, .back-leave-to {
+    transform: translate(100%);
+}
+.forward-leave-to, .back-leave-to {
+    opacity: 0;
+}
 </style>
